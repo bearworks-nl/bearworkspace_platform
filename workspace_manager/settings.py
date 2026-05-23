8de8d4df7fcfc03c4b77945d0bfe41e19717525c
Benno Rummens <benno@rummens.com>
2026-05-23 13:44:39 +0200

    cm

--- workspace_manager/settings.py
+++ workspace_manager/settings.py
@@ -51,6 +51,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'apps.core.context_processors.sidebar_context',
+                'apps.core.context_processors.site_settings',
             ],
         },
     },
